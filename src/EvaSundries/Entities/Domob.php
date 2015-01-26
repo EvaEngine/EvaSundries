b8c80e439de0080694a522262d6767049bcc5cb6
Shao <liujaysen@gmail.com>
2015-01-26 18:05:35 +0800

    domob

--- src/EvaSundries/Entities/Domob.php
+++ src/EvaSundries/Entities/Domob.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Wscn
- *
- * @link      https://github.com/wallstreetcn/wallstreetcn
- * @copyright Copyright (c) 2010-2014 WallstreetCN
- * @author    WallstreetCN Team: shao<liujaysen@gmail.com>
- */
 namespace Eva\EvaSundries\Entities;
 
 //记录多盟请求的数据
